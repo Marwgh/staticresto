2380f57fe8e8006333cdd73df5507bfd684b7fc7
Marwgh <marw0619@stud.kea.dk>
2020-08-31 09:37:32 +0200

    version 31/08

---
+++ script.js
@@ -0,0 +1,54 @@
+
+fetch("https://kea-alt-del.dk/t5/api/productlist")
+.then(function(response){
+    console.log("response")
+    return response.json();
+})
+.then (function(data){
+    console.log(data)
+    datareceived(data);
+})
+
+function datareceived(product){
+    product.forEach(showproduct);
+}
+
+
+function showproduct(product) {
+    const templ = document.querySelector("#Template").content;
+
+    const copy = templ.cloneNode(true);
+
+    copy.querySelector("h1").textContent = product.name;
+    copy.querySelector("span").textContent = product.price;
+    copy.querySelector(".discount").textContent =  "discount: " + product.discount;
+    copy.querySelector(".descript").textContent =  product.shortdescription;
+    if (product.soldout == true ) {
+        copy.querySelector("article").classList.add("soldo");
+    }
+    if (product.vegetarian == true ) {
+        copy.querySelector("article").classList.add("vegana");
+    }
+
+
+    if (product.category == "starter") {
+        document.querySelector("#sectionStarter").appendChild(copy);
+    } else if (product.category == "main") {
+        document.querySelector("#sectionMain").appendChild(copy);
+    } else if (product.category == "drinks") {
+        document.querySelector("#sectionDrinks").appendChild(copy);
+    } else if (product.category == "dessert") {
+        document.querySelector("#sectionDessert").appendChild(copy);
+    }
+
+}
+
+
+/*
+copy.querySelector("img").src= product.image;
+copy.querySelector(".dob").textContent = hero.dob.year +"/" + hero.dob.month +"/"+ hero.dob.day ;
+    if (hero.wearsACape==true) {
+        copy.querySelector(".cape").textContent = "wears a cape";
+    } else  {
+        copy.querySelector(".cape").textContent = "does not wears a cape";
+    }*/
